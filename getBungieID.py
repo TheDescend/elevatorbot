250d19cf896773242776225bd65dc8aa8fc6a30c
LukasSchmid97 <47521071+LukasSchmid97@users.noreply.github.com>
2019-10-10 11:02:30 +0200

    Generalized and extracted clanid and displayName

--- getBungieID.py
+++ getBungieID.py
@@ -1,6 +1,9 @@
 
 import requests
 
+clanid = '2784110'
+displayName = 'Hali'
+
 baseURL = "https://www.bungie.net/Platform"
 PARAMS = {'X-API-Key':'3743ea53f4014677a36be515239869b3'}
 
@@ -8,7 +11,7 @@ def getJSONfromURL(requestURL):
     r=requests.get(url=baseURL + requestURL, headers=PARAMS)
     return r.json()
 
-requestURL = "/GroupV2/2784110/members/" #bloodoak memberlist
+requestURL = "/GroupV2/" + clanid + "/members/"
 memberJSON = getJSONfromURL(requestURL)
 memberlist = memberJSON['Response']['results']
 memberids = dict()
@@ -18,5 +21,5 @@ for member in memberlist:
 # memberids['Hali'] is my destinyMembershipID
 # uses lastSeenDisplayName
 
-bungieID = memberids['Hali']
+bungieID = memberids[displayName]
 print(bungieID)
