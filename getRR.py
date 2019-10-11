f1a05d0018db255b963815de6408fd90b54c98d8
Lukas Schmid <lukasschmid97@gmail.com>
2019-10-11 20:28:34 +0200

    whoops

--- getRR.py
+++ getRR.py
@@ -1,9 +1,8 @@
-
-import requests
+import requests,config
 
 bungieID = input('giff bungieNet-ID\n')
 
-PARAMS = {'X-API-Key':'3743ea53f4014677a36be515239869b3'}
+PARAMS = {'X-API-Key':config.key}
 url = 'https://www.bungie.net/platform/User/GetMembershipsById/{}/3/'.format(bungieID)
 r=requests.get(url=url, headers=PARAMS)
 print('https://raid.report/pc/' + r.json()['Response']['destinyMemberships'][0]['membershipId'])
