736716e3461ed0be0c378ad506133eefd33fa81c
Lukas Schmid <luschmi@student.ethz.ch>
2019-10-11 15:24:33 +0200

    all static data into dict.py and refactored main to more elegantly support different types of requirements

---
+++ dict.py
@@ -0,0 +1,228 @@
+import requests, zipfile, os, pickle, json, sqlite3
+
+getNameFromHashRecords = {}
+getNameFromHashActivity= {}
+getNameFromHashCollectible = {}
+
+# from https://data.destinysets.com/
+spirePHashes = ['3213556450']
+spireHashes = ['119944200']
+eaterPHashes = ['809170886']
+eaterHashes = ['3089205900']
+leviHashes = ['2693136600','2693136601','2693136602','2693136603','2693136604','2693136605'] #there's like 12 of each
+leviPHashes = ['417231112','508802457','757116822', '771164842', '1685065161', '1800508819'
+                ,'2449714930','3446541099','3857338478','3879860661', '3912437239','4206123728']
+scourgeHashes = ['548750096'] 
+lwHashes = ['2122313384']
+cosHashes = ['3333172150'] 
+
+requirementHashes = {
+    'Y1':{
+        'Spire': {
+            'requirements': ['clears'],
+            'clears': [
+                {'count' : 4,
+                'actHashes' : spireHashes}, #normal
+                {'count' : 2,
+                'actHashes' : spirePHashes} #prestige 
+            ]
+        },
+        'Spire Master': {
+            'requirements': ['clears','flawless'],
+            'clears': [
+                {'count' : 15,
+                'actHashes' : spirePHashes}  #prestige
+            ],
+            'flawless' : spireHashes + spirePHashes
+        },
+        'Eater': {
+            'requirements': ['clears'],
+            'clears': [
+                {'count' : 4,
+                'actHashes' : eaterHashes}, #normal
+                {'count' : 2,
+                'actHashes' : eaterPHashes} #prestige
+            ]
+        },
+        'Eater Master': {
+            'requirements': ['clears','flawless'],
+            'clears': [
+                {'count' : 15,
+                'actHashes' : eaterPHashes}
+            ], #prestige
+            'flawless': eaterHashes + eaterPHashes
+        },
+        'Levi': {
+            'requirements': ['clears'],
+            'clears': [
+                {'count' : 4,
+                'actHashes' : leviHashes}, #normal
+                {'count' : 2,
+                'actHashes' : leviPHashes} #prestige
+            ]
+        },
+        'Levi Master': {
+            'requirements': ['clears','flawless','collectibles'],
+            'clears':[
+                {'count' : 15,
+                'actHashes' : leviPHashes}, #prestige
+            ],
+            'flawless': leviHashes + leviPHashes,
+            'collectibles': [
+                '1766893932', #good dog
+                '1766893933', #splish splash
+                '1766893935', #two enter, one leaves
+                '1766893934'  #take the throne
+            ]
+        }
+    },
+    'Y2':{
+        'Crown': {
+            'requirements': ['clears','records'],
+            'clears':[
+                {'count' : 15,
+                'actHashes' : cosHashes}
+            ], #• Minimum 15 full clears
+            'records': [
+                '1575460004', #• Limited Blessings
+                '1575460003', #• Total Victory
+                '1575460002'#• With Both Hands
+           ]
+       },
+        'Crown Master': {
+            'requirements': ['clears','records'],
+            'clears': [
+                {'count' : 30,
+                'actHashes' : cosHashes}, #• Minimum 15 full clears
+            ],
+            'records': [
+                '1558682416', #Crown of Ease [Flawless]
+                '1558682417', #• Arc Borne :Arc: 
+                '1558682418', #• Void Borne :Void: 
+                '1558682419', #• Solar Borne :Solar: 
+                '1558682428' #• Stay Classy [Same Class]
+            ]
+        },
+        'Scourge': {
+            'requirements': ['clears','records'],
+            'clears': [
+                {'count' : 15,
+                'actHashes' : scourgeHashes}
+            ],
+            'records':[
+                '1428463716', #All for one, one for all
+                '1804999028', #hold the line
+                '4162926221'  #to each their own
+            ]
+        },
+        'ScourgeMaster': {
+            'requirements': ['clears','records'],
+            'clears': [
+                {'count' : 30,
+                'actHashes' : scourgeHashes}
+            ],
+            'records': [
+            '2648109757', #Like a diamond
+            '772878705',  #solarstruck
+            '496309570',  #voidstruck
+            '105811740',  #thunderstruck
+            '3780682732',  #stay classy
+            ]
+        },
+        'Last Wish':{
+            'requirements': ['clears','records'],
+            'clears' :[
+                {'count' : 15,
+                'actHashes' : lwHashes},   #• Minimum 15 full clears
+            ],
+            'records':[
+                '2822000740', #• Summoning Ritual
+                '2196415799', #• Coliseum Champion
+                '1672792871', #• Forever Fight
+                '149192209', #• Keep Out
+                '3899933775' #• Strength of Cheesery
+            ]
+        },
+        'Last Wish Master': {
+            'requirements': ['clears','records'],
+            'clears': [
+                {'count' : 30,
+                'actHashes' : lwHashes}
+            ], #Minimum 15 full clears
+            'records': [
+                '4177910003', #Petra's Run [Flawless]
+                '3806804934', #Thunderstruck :Arc:
+                '567795114', #The New Meta [Same Class]
+                '1373122528', #Night Owl :Void:
+                '2398356743' #Sunburn :Solar:
+            ]
+        }
+    },
+    'Y3': {
+        'Garden':{},
+        'Garden Master':{}
+    }
+
+}
+
+platform = {
+    1: 'Xbox',
+    2: 'Playstation',
+    3: 'Steam',
+    4: 'Blizzard',
+    5: 'Stadia',
+    10: 'Demon',
+    254: 'BungieNext'
+}
+
+def getManifest():
+    manifest_url = 'http://www.bungie.net/Platform/Destiny2/Manifest/'
+
+    #get the manifest location from the json
+    r = requests.get(manifest_url)
+    manifest = r.json()
+    #print(manifest['Response'].keys())
+    mani_url = 'http://www.bungie.net'+manifest['Response']['mobileWorldContentPaths']['en']
+
+    #Download the file, write it to 'MANZIP'
+    r = requests.get(mani_url)
+    with open("MANZIP", "wb") as zip:
+        zip.write(r.content)
+    #print("Download Complete!")
+
+    #Extract the file contents, and rename the extracted file
+    # to 'Manifest.content'
+    with zipfile.ZipFile('MANZIP') as zip:
+        name = zip.namelist()
+        zip.extractall()
+    os.rename(name[0], 'Manifest.content')
+
+def fillDictFromDB(dictRef, table):
+    if not os.path.exists(table + '.json'): 
+        if not os.path.exists('Manifest.content'):
+            getManifest()
+        con = sqlite3.connect('manifest.content')
+        cur = con.cursor()
+
+
+        cur.execute(
+        '''SELECT 
+            json
+        FROM 
+        ''' + table
+        )
+        items = cur.fetchall()
+        item_jsons = [json.loads(item[0]) for item in items]
+        con.close()
+        for ijson in item_jsons:
+            if 'name' in ijson['displayProperties'].keys():
+                dictRef[ijson['hash']] = ijson['displayProperties']['name']
+        with open(table + '.json', 'w') as outfile:
+            json.dump(dictRef, outfile)
+    else:
+        with open(table + '.json') as json_file:
+            dictRef.update(json.load(json_file))
+
+fillDictFromDB(getNameFromHashRecords, 'DestinyRecordDefinition')
+fillDictFromDB(getNameFromHashActivity, 'DestinyActivityDefinition')
+fillDictFromDB(getNameFromHashCollectible, 'DestinyCollectibleDefinition')
--- main.py
+++ main.py
@@ -1,173 +1,44 @@
 import requests, zipfile, os, sys, pickle, json, sqlite3, pandas, openpyxl, xlsxwriter, time
-import config, setupdics
+import config
 
-from setupdics import getNameFromHashRecords, getNameFromHashActivity, getNameFromHashCollectible
+from dict import *
+
+# pylint: disable=W0223
+# pylint: disable=abstract-class-instantiated
 
 globalStart = time.perf_counter()
 globalIntervals = globalStart
 
-# from https://data.destinysets.com/
-spirePHash = '3213556450'
-spireHash = '119944200'
-eaterPHash = '809170886'
-eaterHash = '3089205900'
-leviHashes = ['2693136600','2693136601','2693136602','2693136603','2693136604','2693136605'] #there's like 12 of each
-leviPHashes = ['417231112','508802457','757116822', '771164842', '1685065161', '1800508819'
-                ,'2449714930','3446541099','3857338478','3879860661', '3912437239','4206123728']
-scourgeHash = '548750096' #getHashesFromNameActivity('Scourge of the Past')
-lwHash = '2122313384' #getHashesFromNameActivity('Last Wish: Level 55')
-cosHash = '3333172150' #getHashesFromNameActivity('Crown of Sorrow: Normal')
-
-requirementHashes = {
-    'Y1':{
-        'Spire': [
-            {'clears' : 4,
-            'actHash' : spireHash}, #normal
-            {'clears' : 2,
-            'actHash' : spirePHash} #prestige 
-            ],
-        'Spire Master': [
-            {'clears' : 15,
-            'actHash' : spirePHash}, #prestige
-            {'flawless' : spireHash,
-            'flawlessP' : spirePHash}
-        ],
-
-        'Eater': [
-            {'clears' : 4,
-            'actHash' : eaterHash}, #normal
-            {'clears' : 2,
-            'actHash' : eaterPHash} #prestige
-            ],
-        'Eater Master': [
-            {'clears' : 15,
-            'actHash' : '809170886'}, #prestige
-            {'flawless' : eaterHash,
-            'flawlessP' : eaterPHash}
-        ],
-        'Levi': [
-            {'clears' : 4,
-            'actHash' : leviHashes}, #normal
-            {'clears' : 2,
-            'actHash' : leviPHashes} #prestige
-            ],
-        'Levi Master': [
-            {'clears' : 15,
-            'actHash' : leviPHashes}, #prestige
-            {'flawless' : leviHashes,
-            'flawlessP' : leviPHashes},
-            {'collectible' : '1766893932'}, #good dog
-            {'collectible' : '1766893933'}, #splish splash
-            {'collectible' : '1766893935'}, #two enter, one leaves
-            {'collectible' : '1766893934'}  #take the throne
-        ]
-    },
-    'Y2':{
-        'Crown': [
-            {'clears' : 15,
-            'actHash' : '3333172150'}, #• Minimum 15 full clears
-            '1575460004', #• Limited Blessings
-            '1575460003', #• Total Victory
-            '1575460002'#• With Both Hands
-        ],
-        'Crown Master': [
-            {'clears' : 30,
-            'actHash' : '3333172150'}, #• Minimum 15 full clears
-            '1558682416', #Crown of Ease [Flawless]
-            '1558682417', #• Arc Borne :Arc: 
-            '1558682418', #• Void Borne :Void: 
-            '1558682419', #• Solar Borne :Solar: 
-            '1558682428' #• Stay Classy [Same Class]
-        ],
-        'Scourge': [
-            {'clears' : 15,
-            'actHash' : '548750096'},
-            '1428463716', #All for one, one for all
-            '1804999028', #hold the line
-            '4162926221'  #to each their own
-        ],
-
-        'ScourgeMaster': [
-            {'clears' : 30,
-            'actHash' : '548750096'},
-            '2648109757', #Like a diamond
-            '772878705',  #solarstruck
-            '496309570',  #voidstruck
-            '105811740',  #thunderstruck
-            '3780682732',  #stay classy
-            ],
-        'Last Wish':[
-            {'clears' : 15,
-            'actHash' : '2122313384'},   #• Minimum 15 full clears
-            '2822000740', #• Summoning Ritual
-            '2196415799', #• Coliseum Champion
-            '1672792871', #• Forever Fight
-            '149192209', #• Keep Out
-            '3899933775' #• Strength of Cheesery
-        ],
-
-        'Last Wish Master': [
-            {'clears' : 15,
-            'actHash' : '2122313384'}, #Minimum 30 full clears
-            '4177910003', #Petra's Run [Flawless]
-            '3806804934', #Thunderstruck :Arc:
-            '567795114', #The New Meta [Same Class]
-            '1373122528', #Night Owl :Void:
-            '2398356743' #Sunburn :Solar:
-        ]
-    },
-    'Y3': {
-        'Garden':[],
-        'Garden Master':[]
-    }
-
-}
-
 #sys.stdout.errors = 'replace'
 baseURL = "https://www.bungie.net/Platform"
 PARAMS = {'X-API-Key': config.key}
-writer = pandas.ExcelWriter('clanAchievements.xlsx', engine='xlsxwriter')
+path = os.path.dirname(os.path.abspath(__file__))
+writer = pandas.ExcelWriter(path + '\\clanAchievements.xlsx', engine='xlsxwriter') # pylint: disable=W0223
 
-platform = {
-    1: 'Xbox',
-    2: 'Playstation',
-    3: 'Steam',
-    4: 'Blizzard',
-    5: 'Stadia',
-    10: 'Demon',
-    254: 'BungieNext'
-}
 jsondict = {}
 def getJSONfromURL(requestURL, baseURL=baseURL, console=False):
     #jstart = time.perf_counter()
     if requestURL in jsondict:
         return jsondict[requestURL]
-    r=None
-    while(r == None or r.status_code != requests.codes[r'\o/']):
-        try:
-            r = requests.get(url=baseURL + str(requestURL), headers=PARAMS)
-            if r.status_code == 200:
-                jsondict[requestURL] = r.json()
-                break
-            if r.status_code == 400:
-                if console:
-                    return None
-                print(requestURL + ' not on steam')
-                for i in [1,2,4,5,10,254]:
-                    #print('/Destiny2/' + str(i) + requestURL[11:])
-                    console = getJSONfromURL('/Destiny2/' + str(i) + requestURL[11:], baseURL=baseURL, console=True)
-                    if console is not None:
-                        print('found on ' + platform.get(i))
-                        jsondict[requestURL] = console
-                        return console
-                return None
-            else:#
-                print('request for ' + baseURL + requestURL + ' failed with code ' + str(r.status_code))
+    r = requests.get(url=baseURL + str(requestURL), headers=PARAMS)
+    if r.status_code == 200:
+        jsondict[requestURL] = r.json()
+        return r.json()
+    elif r.status_code == 400:
+        if console:
+            return None
+        print(requestURL + ' not on steam')
+        for i in [1,2,4,5,10,254]:
+            #print('/Destiny2/' + str(i) + requestURL[11:])
+            console = getJSONfromURL('/Destiny2/' + str(i) + requestURL[11:], baseURL=baseURL, console=True)
+            if console is not None:
+                print('found on ' + platform.get(i))
+                jsondict[requestURL] = console
+                return console
+        return None
+    else:#
+        print('request for ' + baseURL + requestURL + ' failed with code ' + str(r.status_code))
 
-        except requests.exceptions.RequestException as e:
-            print(e)
-            print('getting ' + baseURL + requestURL + ' failed with exception ' + e.strerror)
-            r=None
     #jend = time.perf_counter()
     #print('json request time:', jend - jstart)
     return r.json()
@@ -218,97 +89,76 @@ for member in memberlist:
 
 # memberids['Hali'] is my destinyMembershipID
 userRoles = {}
-for year in requirementHashes:
-    result = {}
+for year,yeardata in requirementHashes.items():
+    yearResult = {}
     for username, userid in memberids.items():
         if not username in userRoles.keys():
             userRoles[username] = []
         #print('time used:', time.perf_counter() - globalIntervals)
-        globalIntervals = time.perf_counter()
-        print(year + ' processing user: ' + username + ' with id ' + userid)
-    
-        requestURL = "/Destiny2/3/Profile/" + userid + "?components=900" #3 = steam
+        #globalIntervals = time.perf_counter()
+        print('Processing user: ' + username + ' with id ' + userid)
+        system = 3 #= steam
+        requestURL = "/Destiny2/"+ str(system) + "/Profile/" + userid + "?components=900" 
         achJSON = getJSONfromURL(requestURL)    
         triumphs = achJSON['Response']['profileRecords']['data']['records']
         
-        result[username] = {}
-        if achJSON is None:
-                break
-        yearReqs = requirementHashes[year]
-        for role in yearReqs:
-            roleReqs = yearReqs[role]
-            rolestatus = len(roleReqs) > 0
-            for rHash in roleReqs:
-                if isinstance(rHash, dict):
-                    if 'flawless' in rHash.keys():
-                        flawlessL = flawlessList(userid)
-                        if isinstance(rHash['flawless'], list):
-                            condition = 'flawless ' + getNameFromHashActivity[rHash['flawless'][0]]
-                            found = False
-                            for h in rHash['flawless']:
-                                if h in flawlessL:
-                                    result[username][condition] = 'True'
-                                    found = True
-                            for h in rHash['flawlessP']:
-                                if h in flawlessL:
-                                    result[username][condition] = 'True'
-                                    found = True
-                            if not found:
-                                rolestatus = False
-                                result[username][condition] = 'False'
-                        elif rHash['flawless'] in flawlessL:
-                            condition = 'flawless ' + getNameFromHashActivity[rHash['flawless']]
-                            result[username][condition] = 'True'
-                        elif rHash['flawlessP'] in flawlessL:
-                            condition = 'flawless ' + getNameFromHashActivity[rHash['flawlessP']]
-                            result[username][condition] = 'True'
-                        else:
-                            condition = 'flawless ' + getNameFromHashActivity[rHash['flawless']]
-                            result[username][condition] = 'False'
-                            rolestatus = False
-                        continue
-                    elif 'collectible' in rHash.keys():
-                        condition = getNameFromHashCollectible[rHash['collectible']]
-                        if playerHasCollectible(userid, rHash['collectible']):
-                            result[username][condition] = 'True'
-                        else:
-                            result[username][condition] = 'False'
-                            rolestatus = False
-                        continue
-                    else:
-                        actHash = rHash['actHash']
-                        requiredN = rHash['clears']
-                        activityname = ''
-                        if isinstance(actHash, list):
-                            cc = 0
-                            for h in actHash:
-                                cc += getClearCount(userid, h)
-                            activityname = getNameFromHashActivity[str(actHash[0])]
-                        else:
-                            cc = getClearCount(userid, rHash['actHash'])
-                            activityname = getNameFromHashActivity[str(actHash)]
+        yearResult[username] = {}
+        
+        for role,roledata in yeardata.items():
+            print('checking role ' + role)
+            rolestatus = len(roledata) > 0
+            if not 'requirements' in roledata:
+                print('malformatted requirementHashes')
+                continue
+            for req in roledata['requirements']:
+                if req == 'clears':
+                    creq = roledata['clears']
+                    for raid in creq:
+                        requiredN = raid['count']
+                        cc = 0
+                        for h in raid['actHashes']:
+                            cc += getClearCount(userid, h)
+                        activityname = getNameFromHashActivity[str(raid['actHashes'][0])]
                         
                         condition = activityname + ' clears (' + str(requiredN) + ')'
-                        result[username][condition] = str(cc >= requiredN) + ' (' + str(cc) + ')'
+                        yearResult[username][condition] = str(cc >= requiredN) + ' (' + str(cc) + ')'
                         rolestatus &= (str(cc >= requiredN) == 'True')
-                        continue
-                else: #rhash hash and not dict
-                    #print(type(rHash))
-                    status = True
-                    name = getNameFromHashRecords[rHash]
-                    for part in triumphs[rHash]['objectives']:
-                        status &= (str(part['complete']) == 'True')
-                    result[username][name] = str(status)
-                    rolestatus &= (str(status) == 'True')
-            result[username][role] = str(rolestatus)
+                elif req == 'flawless':
+                    flawlessL = flawlessList(userid)
+                    condition = 'flawless ' + getNameFromHashActivity[roledata['flawless'][0]]
+                    found = False
+                    for h in roledata['flawless']:
+                        if h in flawlessL:
+                            yearResult[username][condition] = 'True'
+                            found = True
+                    if not found:
+                        rolestatus = False
+                        yearResult[username][condition] = 'False'
+                elif req == 'collectibles':
+                    for collectible in roledata['collectibles']:
+                        condition = getNameFromHashCollectible[collectible]
+                        if playerHasCollectible(userid, collectible):
+                            yearResult[username][condition] = 'True'
+                        else:
+                            yearResult[username][condition] = 'False'
+                            rolestatus = False
+                elif req == 'records':
+                    for record in roledata['records']:
+                        status = True
+                        condition = getNameFromHashRecords[record]
+                        for part in triumphs[record]['objectives']:
+                            status &= (str(part['complete']) == 'True')
+                        yearResult[username][condition] = str(status)
+                        rolestatus &= (str(status) == 'True')
+
+            yearResult[username][role] = str(rolestatus)
             if rolestatus:
                 userRoles[username].append(role)
             else:
                 userRoles[username].append('')
 
-    df = pandas.DataFrame(result)
+    df = pandas.DataFrame(yearResult)
     df = df.transpose()
-
 
     df.to_excel(writer, sheet_name = year + ' Roles')
 
@@ -324,21 +174,21 @@ importantColumns = {
     'Y2'  : ['A','F', 'M','R','X','AE','AK'],
     'Y3'  : ['A']
 }
-color_range = "A2:ZZ300".format(298)
 
 worksheet = writer.sheets['User Roles']
-worksheet.set_column('A:M', 6, fat)
+worksheet.set_column('A:A', 15, fat)
+worksheet.set_column('B:M', 6, fat)
 
 for year in requirementHashes:
     worksheet = writer.sheets[year + ' Roles']
     worksheet.set_column('A:AK', 2)
     for let in importantColumns[year]:
         worksheet.set_column(let +':'+let, 15, fat)
-    worksheet.conditional_format(color_range, {'type': 'text',
+    worksheet.conditional_format("A2:ZZ300", {'type': 'text',
                                                 'criteria': 'containing',
                                                 'value': 'False',
                                                 'format': redBG})
-    worksheet.conditional_format(color_range, {'type': 'text',
+    worksheet.conditional_format("A2:ZZ300", {'type': 'text',
                                                 'criteria': 'containing',
                                                 'value': 'True',
                                                 'format': greenBG})
