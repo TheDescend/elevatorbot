139d3abba4530366430e10830d0e231172beea01
Lukas Schmid <lukasschmid97@gmail.com>
2019-10-21 14:43:32 +0200

    remove a lot of unnecessary comments from Spreasheet2

--- Spreadsheet2.py
+++ Spreadsheet2.py
@@ -1,16 +1,14 @@
 import os, sys, json, pandas, openpyxl, xlsxwriter
 import pandas.io.formats.excel
 import config
-from functions import getNameToHashMapByClanid,getTriumphsJSON, playerHasClears, getClearCount,playerHasFlawless,playerHasCollectible,playerHasTriumph,playerHasRole,getPlayerRoles
-from dict import requirementHashes, getNameFromHashActivity,getNameFromHashCollectible,getNameFromHashRecords
+from functions import getNameToHashMapByClanid,getPlayerRoles
+from dict import requirementHashes
 
 # pylint: disable=W0223
 # pylint: disable=abstract-class-instantiated
 
 path = os.path.dirname(os.path.abspath(__file__))
 writer = pandas.ExcelWriter(path + '\\clanAchievementsShort.xlsx', engine='xlsxwriter') # pylint: disable=W0223
-
-#print(playerHasRole(4611686018468695677,'Levi Master','Y1'))
 
 clanids = [
     2784110, #Bloodoak I
@@ -40,8 +38,6 @@ for clanid in clanids:
                 table[username].append('+')
             else:
                 table[username].append('-')
-    #print(table)
-    #print(rolelist)
     df = pandas.DataFrame()
     df = df.from_dict(table, orient='index', columns=rolelist)
     columnlist=df.columns
@@ -52,10 +48,6 @@ for clanid in clanids:
         worksheet.write(0, idx, val)
 print('generating sheets...')
 
-
-#workbook = xlsxwriter.Workbook(path + '\\clanAchievementsShort.xlsx')
-
-
 workbook = writer.book
 bold = workbook.add_format({'bold': True})
 
@@ -63,7 +55,6 @@ redBG = workbook.add_format({'bg_color': '#FFC7CE'})
 greenBG = workbook.add_format({'bg_color': '#C6EFCE'})
 
 verticalRole = workbook.add_format({'bold': True, 'rotation': 90})
-#verticalRole.set_rotation(90)
 
 for wname, worksheet in writer.sheets.items():
     worksheet.set_landscape()
@@ -77,39 +68,6 @@ for wname, worksheet in writer.sheets.items():
                                                 'criteria': 'containing',
                                                 'value': '+',
                                                 'format': greenBG})
-    #print(wname + ' set')
-#worksheet.write(0, 0, 'This text is rotated', verticalRole)
-#worksheet.set_column('F:M', 6, verticalRole)
 writer.save()
 
 print('excel file created')
-# for year,yeardata in requirementHashes.items():
-#     curCol = 0
-#     worksheet.set_column(curCol, curCol, 15, bold)
-#     worksheet = writer.sheets[year + ' Roles']
-#     worksheet.set_column('A:AK', 2, None)
-#     for role,roledata in yeardata.items():
-#         curCol += 1
-#         for reqC, reqData in roledata.items():
-#             if reqC == 'flawless':
-#                 curCol += 1
-#                 #print('+1 for flawless')
-#             elif not reqC == 'requirements' and not reqC == 'replaced_by':
-#                 curCol += len(reqData)
-#                 #print(f'+{len(reqData)} for {reqC}')
-#                 #print(f'req {reqC} has length {len(reqData)} and content {reqData}')
-#                 #print(f'added req, curCol now {curCol}')
-        
-#         worksheet.set_column(curCol, curCol, 15, bold)
-#         #print(f'{curCol} is bold.')
-
-#     worksheet.conditional_format("A2:ZZ300", {'type': 'text',
-#                                                 'criteria': 'containing',
-#                                                 'value': 'False',
-#                                                 'format': redBG})
-#     worksheet.conditional_format("A2:ZZ300", {'type': 'text',
-#                                                 'criteria': 'containing',
-#                                                 'value': 'True',
-#                                                 'format': greenBG})
-#     worksheet.set_zoom(80)
-
