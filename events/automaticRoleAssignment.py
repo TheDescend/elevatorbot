efe0255b4408823ab706a6aa3780f18ba525701e
Lukas Schmid <lukasschmid97@gmail.com>
2020-02-14 09:43:14 +0100

    role update ignores bots and other servers

--- events/automaticRoleAssignment.py
+++ events/automaticRoleAssignment.py
@@ -18,24 +18,24 @@ class AutomaticRoleAssignment(BaseEvent):
     async def run(self, client):
         newtonslab = client.get_channel(670637036641845258)
         await newtonslab.send('running role update...')
-        for guild in client.guilds:
-            for discordUser in guild.members:
-                destinyID = getUserMap(discordUser.id)
-                if not destinyID:
-                    destinyID = getUserIDbySnowflakeAndClanLookup(discordUser,fullMemberMap)
-                    if destinyID:
-                        await newtonslab.send(f'''Hi! User {discordUser.name} aka {discordUser.nick} was not found in my database!\n 
-                        If his rr is https://raid.report/pc/{pcid} please use ```!forceregister {discordUser.id} {pcid}```''')
-                    else: 
-                        await newtonslab.send(f'''Hi! User {discordUser.name} aka {discordUser.nick} was not found in my database!\n 
-                        Please use ```!forceregister {discordUser.id} <destinyID>``` so he can get synced next time''')
-                        continue
-                (newRoles, removeRoles) = getPlayerRoles(destinyID, [role.name for role in discordUser.roles])
-                await assignRolesToUser(newRoles, discordUser, guild)
-                await removeRolesFromUser(removeRoles, discordUser, guild)
+        guild = newtonslab.guild
+        for discordUser in guild.members:
+            destinyID = getUserMap(discordUser.id)
+            if not destinyID:
+                destinyID = getUserIDbySnowflakeAndClanLookup(discordUser,fullMemberMap)
+                if destinyID:
+                    await newtonslab.send(f'''Hi! User {discordUser.name} aka {discordUser.nick} was not found in my database!\n 
+                    If his rr is https://raid.report/pc/{pcid} please use ```!forceregister {discordUser.id} {pcid}```''')
+                elif not discordUser.bot: 
+                    await newtonslab.send(f'''Hi! User {discordUser.name} aka {discordUser.nick} was not found in my database!\n 
+                    Please use ```!forceregister {discordUser.id} <destinyID>``` so he can get synced next time''')
+                    continue
+            (newRoles, removeRoles) = getPlayerRoles(destinyID, [role.name for role in discordUser.roles])
+            await assignRolesToUser(newRoles, discordUser, guild)
+            await removeRolesFromUser(removeRoles, discordUser, guild)
 
-                for clanid, rolename in clanids.items():
-                    if isUserInClan(destinyID,clanid):
-                        await assignRolesToUser([rolename],discordUser,guild)
-                    else:
-                        await removeRolesFromUser([rolename], discordUser, guild)
+            for clanid, rolename in clanids.items():
+                if isUserInClan(destinyID,clanid):
+                    await assignRolesToUser([rolename],discordUser,guild)
+                else:
+                    await removeRolesFromUser([rolename], discordUser, guild)
