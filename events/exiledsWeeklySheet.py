58032450e62ee6156ca36236a06baa2199e0b989
Lukas Schmid <lukasschmid97@gmail.com>
2020-03-09 16:55:36 +0100

    It's also Nerias weekly sheet now

--- events/exiledsWeeklySheet.py
+++ events/exiledsWeeklySheet.py
@@ -18,10 +18,14 @@ class ExiledsWeeklySheet(BaseEvent):
         greetings = ['Have a nice week', 'Enjoy the grind!', 'You\'re doing great', 'Thanks for everything', 'The clan appreciates you', 'You can achieve your goals', 'Someone cares', 'Life is like a Jötunn', 'Enjoy the little things']
         print('generating and sending exileds sheet')
         exiled = client.get_user(206878830017773568)
+        neria = client.get_user(109022023979667456)
         sheetpath = createSheet()
         if sheetpath is not None:
             f = discord.File(open(sheetpath,'rb'),'AchievementSheet.xlsx')
             await exiled.send(file=f)
             await exiled.send(random.choice(greetings))
+
+            await neria.send(file=f)
+            await neria.send(random.choice(greetings))
         else:
             await exiled.send('something happened to the bot, sorry')
