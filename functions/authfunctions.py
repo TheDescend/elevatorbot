a7fef0e7bc988a99102344b1d6dab6e288859c1a
lukasschmid97 <lukasschmid97@gmail.com>
2020-05-08 08:46:34 +0000

    Updated spider to include owned materials

--- functions/authfunctions.py
+++ functions/authfunctions.py
@@ -1,7 +1,8 @@
-from functions.database     import getToken
+from functions.database     import getToken, lookupDiscordID
 from oauth                  import refresh_token
 from static.config          import BUNGIE_TOKEN
 from functions.network      import getJSONfromURL
+from static.dict            import getNameFromHashInventoryItem
 
 import requests
 session = requests.Session()
@@ -12,7 +13,7 @@ def getJSONwithToken(url, discordID):
     token = getToken(discordID)
     if not token:
         return None
-
+    #print(f'using {token}')
     headers = {'Authorization': f'Bearer {token}', 'x-api-key': BUNGIE_TOKEN, 'Accept': 'application/json'}
     r = session.get(url, headers = headers)
     
@@ -35,6 +36,15 @@ def getJSONwithToken(url, discordID):
     #print(f'Tokenfunction returned {res}')
     return res
 
+def getUserMaterials(destinyID):
+    system = 3
+    url = f'https://stats.bungie.net/Platform/Destiny2/{system}/Profile/{destinyID}/?components=600'
+    res = getJSONwithToken(url, lookupDiscordID(destinyID))
+    if not res:
+        return None
+    materialdict = list(res['Response']['characterCurrencyLookups']['data'].values())[0]['itemQuantities']
+    return materialdict
+
 def getSpiderMaterials(discordID, destinyID, characterID):
     """ Gets spiders current selling inventory, requires OAuth"""
 
@@ -49,9 +59,21 @@ def getSpiderMaterials(discordID, destinyID, characterID):
     sales = res['Response']['sales']['data']
     itemhashurl = 'https://bungie.net/platform/Destiny2/Manifest/DestinyInventoryItemDefinition/{hashIdentifier}/'
     returntext = ''
+    usermaterialdict = getUserMaterials(destinyID)
+    usermaterialreadabledict = {}
+    #print(getNameFromHashInventoryItem.keys())
+    for key,value in usermaterialdict.items():
+        if not str(key) in getNameFromHashInventoryItem:
+            print(key)
+            continue
+        materialname = getNameFromHashInventoryItem[str(key)]
+        usermaterialreadabledict[materialname] = value
+    #print(usermaterialreadabledict)
+
     for sale in sales.values():
         soldhash = sale["itemHash"]
         pricehash = sale["costs"][0]["itemHash"]
+        ownedamount = 0
 
         #requests to identify the items TODO save manuscript locally and look them up there?
         soldurl = itemhashurl.format(hashIdentifier=soldhash)
@@ -62,10 +84,35 @@ def getSpiderMaterials(discordID, destinyID, characterID):
         soldname = r['Response']['displayProperties']['name'][9:]
         if 'traitIds' in r['Response'] and 'item_type.bounty' in r['Response']['traitIds']:
             continue
+
+        #print(r['Response'])
         
         #get the name of the asked material
-        r = getJSONfromURL(priceurl)
-        pricename = r['Response']['displayProperties']['name']
+        pricename = getNameFromHashInventoryItem[str(pricehash)]
 
-        returntext += f'selling {sale["quantity"]} {soldname} for {sale["costs"][0]["quantity"]} {pricename}\n'
+        if soldname in usermaterialreadabledict.keys():
+            ownedamount = usermaterialreadabledict[soldname]
+        else:
+            soldnamecut = soldname[:-1]
+            if soldnamecut in usermaterialreadabledict.keys():
+                ownedamount = usermaterialreadabledict[soldnamecut]
+            else:
+                print(soldname)
+                print(usermaterialreadabledict.keys())
+            
+            
+
+        #returntext += f'selling {sale["quantity"]} {soldname} for {sale["costs"][0]["quantity"]} {pricename}\n'
+        returntext += f'selling {ownedamount} {soldname} for {pricename}\n'
+
+    returntext = returntext.replace('Dusklight Shard', '<:DusklightShards:620647201940570133>')
+    returntext = returntext.replace('Phaseglass Needle', '<:Phaseglass:620647202418851895>')
+    returntext = returntext.replace('Seraphite', '<:Seraphite:620647202297085992>')
+    returntext = returntext.replace('Legendary Shards', '<:LegendaryShards:620647202003484672>')
+    returntext = returntext.replace('Alkane Dust', '<:AlkaneDust:620647201827454990>')
+    returntext = returntext.replace('Data Lattice', '<:Datalattice:620647202015936536>')
+    returntext = returntext.replace('Simulation Seeds', '<:SimulationSeeds:620647203635200070>')
+    returntext = returntext.replace('Glimmer', '<:Glimmer:620647202007810098>')
+    returntext = returntext.replace('Enhancement Cores', '<:EnhancementCores:620647201596637185>')
+    returntext = returntext.replace('Helium Filaments', '<:HeliumFilaments:707244746493657160>')
     return returntext
