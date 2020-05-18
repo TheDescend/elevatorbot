1df8697e8b89ee0d23e2a42ec74af90bac77614f
lukasschmid97 <lukasschmid97@gmail.com>
2020-05-18 12:36:58 +0000

    finalized The Lie changes, disabled recurring event

--- events/rasputinWatcher.py
+++ events/rasputinWatcher.py
@@ -17,18 +17,19 @@ class rasputinWatcher(BaseEvent):
     # It will be called once every {interval_minutes} minutes
 
     async def run(self, client):
-        if not os.path.exists('database/rasputinData.pickle'):
-            rasputindf = pd.DataFrame(columns=["datetime",'moon','edz', 'io'])
-        else:
-            rasputindf = pd.read_pickle('database/rasputinData.pickle')
-        now = datetime.now()
-        newdict = {
-            'datetime':now
-        }
-        for objname, objprogress, objtotal in getRasputinQuestProgress():
-            newdict[objname.lower()] = objprogress
+        # if not os.path.exists('database/rasputinData.pickle'):
+        #     rasputindf = pd.DataFrame(columns=["datetime",'moon','edz', 'io'])
+        # else:
+        #     rasputindf = pd.read_pickle('database/rasputinData.pickle')
+        # now = datetime.now()
+        # newdict = {
+        #     'datetime':now
+        # }
+        # for objname, objprogress, objtotal in getRasputinQuestProgress():
+        #     newdict[objname.lower()] = objprogress
         
-        rasputindf = rasputindf.append(newdict, ignore_index=True)
-        rasputindf.to_pickle('database/rasputinData.pickle')
+        # rasputindf = rasputindf.append(newdict, ignore_index=True)
+        # rasputindf.to_pickle('database/rasputinData.pickle')
+        pass
 
         
