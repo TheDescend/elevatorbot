49c2c4c579b43fed18e9396400f62d679c36c1b0
lukasschmid97 <lukasschmid97@gmail.com>
2020-06-19 22:06:24 +0000

    refactoring

--- commands/getRR.py
+++ commands/getRR.py
@@ -37,43 +37,35 @@ class RR(BaseCommand):
                     f'User not found, make sure the spelling/id is correct'
                 ))
                 return
-            username = user.name
 
             destinyID = lookupDestinyID(user.id)
-            if destinyID:
-                syscharlist = getSystemAndChars(destinyID)
-                
-                if not syscharlist:
-                    #get them I guess
-                    message.channel.send("Player not yet in db, updating...")
-                    updateDB(destinyID)
-                    syscharlist = getSystemAndChars(destinyID)
-                    if not syscharlist:
-                        print(f'{destinyID} is borked')
-                        await message.channel.send(embed=embed_message(
-                            'Raid Report',
-                            f'Invalid DestinyID {destinyID}'
-                        ))
-                systemID, _ = syscharlist[0]
-                await message.channel.send(embed=embed_message(
-                    'Raid Report',
-                    f'https://raid.report/{rrsystem[systemID]}/{destinyID}'
-                ))
-                return
         else:
             destinyID = lookupDestinyID(message.author.id)
-            if destinyID:
-                systemID, _ = getSystemAndChars(destinyID)[0]
-                await message.channel.send(embed=embed_message(
-                    'Raid Report',
-                    f'https://raid.report/{rrsystem[systemID]}/{destinyID}'
-                ))
-                return
-
-        await message.channel.send(embed=embed_message(
-            'Error',
-            'Name needs to be more specific or is not in Clan'
-        ))
+            
+        if destinyID:
+            syscharlist = getSystemAndChars(destinyID)
+            if not syscharlist:
+                #get them I guess
+                await message.channel.send("Player not yet in db, updating...")
+                updateDB(destinyID)
+                syscharlist = getSystemAndChars(destinyID)
+                if not syscharlist:
+                    print(f'{destinyID} is borked')
+                    await message.channel.send(embed=embed_message(
+                        'Raid Report',
+                        f'Invalid DestinyID {destinyID}'
+                    ))
+            systemID, _ = syscharlist[0]
+            await message.channel.send(embed=embed_message(
+                'Raid Report',
+                f'https://raid.report/{rrsystem[systemID]}/{destinyID}'
+            ))
+            return
+        else:
+            await message.channel.send(embed=embed_message(
+                'Error',
+                'Name needs to be more specific or is not in Clan'
+            ))
 
 
 class DR(BaseCommand):
