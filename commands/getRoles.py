e15bd9f9789aeae37e9359c9c0c78508efb2e63e
Daniel <>
2020-06-22 18:48:01 +0200

    remove setroles and add functionality to getroles

--- commands/getRoles.py
+++ commands/getRoles.py
@@ -26,13 +26,36 @@ class getRoles(BaseCommand):
     # Override the handle() method
     # It will be called every time the command is received
     async def handle(self, params, message, client):
-        if params:
+        # check if message too long
+        if len(params) > 1:
             await message.channel.send(embed=embed_message(
                 'Error',
-                f'You can\'t do this for other users\n Use !setroles {params[0]} instead'
+                'Incorrect formatting, correct usage is: \n\u200B\n `!getroles *<user>`'
             ))
             return
-        destinyID = lookupDestinyID(message.author.id)
+
+        user = message.author
+        if len(params) == 1:
+            admin = discord.utils.get(message.guild.roles, name='Admin')
+            dev = discord.utils.get(message.guild.roles, name='Developer')
+            if admin not in message.author.roles and dev not in message.author.roles and not message.author.id == params[0]:
+                await message.channel.send(embed=embed_message(
+                    'Error',
+                    'You are not allowed to do that'
+                ))
+                return
+
+            ctx = await client.get_context(message)
+            try:
+                user = await discord.ext.commands.MemberConverter().convert(ctx, params[0])
+            except:
+                await message.channel.send(
+                    embed=embed_message(
+                        'Error',
+                        'User not found, make sure the spelling/id is correct'
+                    ))
+                return
+        destinyID = lookupDestinyID(user.id)
 
         if not destinyID:
             await message.channel.send(embed=embed_message(
@@ -44,19 +67,19 @@ class getRoles(BaseCommand):
         updateDB(destinyID)
         
         async with message.channel.typing():
-            roles_at_start = [role.name for role in message.author.roles]
+            roles_at_start = [role.name for role in user.roles]
             (roleList,removeRoles) = getPlayerRoles(destinyID, roles_at_start)
 
-            await assignRolesToUser(roleList, message.author, message.guild)
-            await removeRolesFromUser(removeRoles,message.author,message.guild)
+            await assignRolesToUser(roleList, user, message.guild)
+            await removeRolesFromUser(removeRoles, user, message.guild)
 
             for role in roleList:
                 if role in requirementHashes['Addition']:
-                    await message.author.add_roles(discord.utils.get(message.guild.roles, name=achText))
+                    await user.add_roles(discord.utils.get(message.guild.roles, name=achText))
                 else:
-                    await message.author.add_roles(discord.utils.get(message.guild.roles, name=raiderText))
+                    await user.add_roles(discord.utils.get(message.guild.roles, name=raiderText))
 
-            roles_now = [role.name for role in message.author.roles]
+            roles_now = [role.name for role in user.roles]
 
             old_roles = {}
             new_roles = {}
@@ -86,7 +109,7 @@ class getRoles(BaseCommand):
                 return
 
             embed = embed_message(
-                f"{message.author.name}'s new Roles",
+                f"{user.name}'s new Roles",
                 f'__Previous Roles:__'
             )
             if not old_roles:
@@ -149,56 +172,6 @@ class flawlesses(BaseCommand):
             destinyID = lookupDestinyID(user.id)
             updateDB(destinyID)
             await message.channel.send(getFlawlessList(destinyID))
-
-class setRoles(BaseCommand):
-    def __init__(self):
-        # A quick description for the help message
-        description = "[admin] Assigns the mentioned user his/her earned roles"
-        params = ['user']
-        super().__init__(description, params)
-
-    # Override the handle() method
-    # It will be called every time the command is received
-
-    async def handle(self, params, message, client):
-        ctx = await client.get_context(message)
-        user = await commands.MemberConverter().convert(ctx, params[0])
-        destinyID = lookupDestinyID(user.id)
-        if not destinyID:
-            await message.channel.send(embed=embed_message(
-                'Error',
-                'Didn\'t find the destiny profile, sorry'
-            ))
-            return
-        status_msg = await message.channel.send(embed=embed_message(
-            'Working...',
-            'Updating DB and assigning roles...'
-        ))
-        status_msg = await message.channel.fetch_message(status_msg.id)
-        updateDB(destinyID)
-        
-        async with message.channel.typing():
-            (roleList,removeRoles) = getPlayerRoles(destinyID, [role.name for role in user.roles])
-            
-            await assignRolesToUser(roleList, user, message.guild)
-            await removeRolesFromUser(removeRoles,user,message.guild)
-
-            for role in roleList:
-                if role in requirementHashes['Addition']:
-                    await user.add_roles(discord.utils.get(message.guild.roles, name=achText))
-                else:
-                    await user.add_roles(discord.utils.get(message.guild.roles, name=raiderText))
-            rolesgiven = ', '.join(roleList)
-            if len(rolesgiven) == 0:
-                await status_msg.edit(embed=embed_message(
-                    'Error',
-                    f'You don\'t seem to have any roles.\nIf you believe this is an Error, refer to one of the @Developers\nOtherwise check <#686568386590802000> to see what you could acquire'
-                ))
-                return
-            await status_msg.edit(embed=embed_message(
-                f'{user.name} Roles',
-                f'Added the roles {rolesgiven}'
-            ))
 
 #improvable TODO
 class removeAllRoles(BaseCommand):
