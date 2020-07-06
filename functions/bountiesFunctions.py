563f10d67d7f527ae5ec2c2f5975d55998138168
Daniel <>
2020-07-06 12:26:33 +0200

    general structuring ideas

---
+++ functions/bountiesFunctions.py
@@ -0,0 +1,17 @@
+# Channels:
+# #leaderboards
+# #register
+# #tournament
+
+# I'd call those from bloodbot.py
+def registerMessage(client, channelID):
+    pass
+    # everyone who reacts to this get's bounbties and pings
+
+def giveOutBounties(client):
+    pass
+    # give all signed up members their bounties, maybe every week, as a dm?
+    # that'd be a way to have it run weekly https://stackoverflow.com/questions/43670224/python-to-run-a-piece-of-code-at-a-defined-time-every-day
+
+def startTournament():
+    pass
