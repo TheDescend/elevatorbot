a44fb31ab01d5dbd2e502ec91b558cf7abc73b15
Daniel <daniel.jaek@gmail.com>
2020-09-03 13:14:26 +0200

    add skeleton for bounties

---
+++ functions/bounties/boutiesBountyRequirements.py
@@ -0,0 +1,169 @@
+# what you need to do to complete the counties
+
+bounties = {
+    # raid bounties
+    'Raids': {
+        # new players
+        "new": {
+            "Clear a raid for the first time": {
+
+            },
+
+            "Finish two raids": {
+
+            },
+
+            "Finish two dungeons": {
+
+            }
+        },
+
+        # experienced players
+        "exp": {
+            "Do a raid with this loadout": {
+
+            },
+
+            "Do a dungeon with this loadout": {
+
+            },
+
+            "Finish a raid within the allowed time frame": {
+
+            },
+
+            "Finish a raid with 20 power or more below recommended": {
+
+            },
+
+            "Do a flawless raid": {
+
+            },
+
+            "Do a flawless dungeon": {
+
+            }
+        }
+    },
+
+
+    # general pve bounties
+    'PvE': {
+        # new players
+        "new": {
+            "Finish a strike with high kills and low deaths": {
+
+            },
+
+            "Clear an adventure with this loadout": {
+
+            },
+
+            "Complete a Nightfall: The Ordeal on any difficulty": {
+
+            }
+        },
+
+        # experienced players
+        "exp": {
+            "Finish a strike with high kills and low deaths": {
+
+            },
+
+            "Clear a daily heroic adventure within the allowed time frame": {
+
+            },
+
+            "Complete a Nightfall: The Ordeal with a high score": {
+
+            }
+        }
+    },
+
+
+    # pvp bounties
+    'PvP': {
+        # new players
+        "new": {
+            "Win a crucible game with a positive K/D": {
+
+            },
+
+            "Get a win streak in the crucible": {
+
+            }
+        },
+
+        # experienced players
+        "exp": {
+            "Get a win streak in the crucible": {
+
+            },
+
+            "Win a crucible game with a high K/D": {
+
+            },
+
+            "Complete a crucible game with a high amount of kills": {
+
+            },
+
+            "Complete a crucible game without dying": {
+
+            }
+        }
+    }
+}
+
+
+# bounties where the whole clan competes against each other
+competition_bounties = {
+    # raid bounties
+    'Raids': {
+        "Get the fastest clear of this raid": {
+
+        },
+
+        "Get the fastest clear of this dungeon": {
+
+        },
+
+        "Do a lowman of this raid": {
+
+        },
+
+        "Get the fastest clear of this raid while using abilities only": {
+
+        },
+    },
+
+    # general pve bounties
+    'PvE': {
+        "Get the fastest completion of this daily heroic story mission": {
+
+        },
+
+        "Get the most kills in a gambit match": {
+
+        },
+
+        "Get the highest Nightfall Score": {
+
+        }
+    },
+
+    # pvp bounties
+    'PvP': {
+        "Get the highest kills in any PvP mode": {
+
+        },
+
+        "Get the best K/D in any PvP mode": {
+
+        },
+
+        "Win this weeks PvP tournament": {
+
+        }
+    }
+}
