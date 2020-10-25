656c60283e9341e360015b8cf989123429bc11e7
Daniel <daniel.jaek@gmail.com>
2020-10-25 13:40:33 +0100

    add !whoIsNotRegisteredAndInClan

--- commands/addRolesToRegistered.py
+++ commands/addRolesToRegistered.py
@@ -1,7 +1,9 @@
 from commands.base_command  import BaseCommand
+from functions.network import getJSONfromURL
 from functions.roles        import assignRolesToUser, removeRolesFromUser
-from functions.database     import getToken
+from functions.database import getToken, lookupDiscordID
 from functions.roles import hasAdminOrDevPermissions
+from static.config import CLANID
 from static.globals import registered_role_id, not_registered_role_id
 
 
@@ -47,6 +49,33 @@ class whoIsNotRegistered(BaseCommand):
         people = []
         for member in message.guild.members:
             if not getToken(member.id):
+                people.append(member.name)
+
+        await message.channel.send(", ".join(people))
+
+
+class whoIsNotRegisteredAndInClan(BaseCommand):
+    def __init__(self):
+        # A quick description for the help message
+        description = "Blames ppl who are not registered and are in the d2 clan"
+        topic = "Registration"
+        params = []
+        super().__init__(description, params, topic)
+
+    # Override the handle() method
+    # It will be called every time the command is received
+    async def handle(self, params, message, client):
+        # get all clan members discordID
+        memberlist = []
+        for member in (await getJSONfromURL(f"https://www.bungie.net/Platform/GroupV2/{CLANID}/Members/"))["Response"]["results"]:
+            destinyID = int(member["destinyUserInfo"]["membershipId"])
+            discordID = lookupDiscordID(destinyID)
+            if discordID is not None:
+                memberlist.append(discordID)
+
+        people = []
+        for member in message.guild.members:
+            if not getToken(member.id) and (member.id in memberlist):
                 people.append(member.name)
 
         await message.channel.send(", ".join(people))
