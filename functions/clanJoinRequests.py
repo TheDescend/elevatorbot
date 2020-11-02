8e3292006a1f5cbd8a678ff5de4e61fc2b4e8c5c
Daniel <daniel.jaek@gmail.com>
2020-11-02 23:13:10 +0100

    fix typo

--- functions/clanJoinRequests.py
+++ functions/clanJoinRequests.py
@@ -64,7 +64,7 @@ async def clanJoinRequestMessageReactions(client, user, emoji, channel, channel_
             text = "Send you a clan application"
             embed = embed_message(
                 "Clan Update",
-                f"{user.display_name} with discordID <{user.id}> and destinyID <{destinyID}> has been send a clan invite"
+                f"{user.display_name} with discordID <{user.id}> and destinyID <{destinyID}> was has had been sended a clan invite"
             )
             await newtonslab.send(embed=embed)
         else:
