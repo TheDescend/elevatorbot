70ba4783fe32c0cef7f8bb98b061fec79242c560
Daniel <daniel.jaek@gmail.com>
2020-11-02 17:48:11 +0100

    !statistic channels

--- commands/statistics.py
+++ commands/statistics.py
@@ -20,6 +20,7 @@ class statistic(BaseCommand):
         if not await hasAdminOrDevPermissions(message):
             return
 
+        # outputs info about emote usage
         if params[0] == "emotes":
             a = await message.channel.send("Working, will take a while...")
             lock = asyncio.Lock()
@@ -101,4 +102,63 @@ class statistic(BaseCommand):
             os.remove('emote_usage.png')
 
 
+        # outputs info about channel usage
+        elif params[0] == "channels":
+            a = await message.channel.send("Working, will take a while...")
+            lock = asyncio.Lock()
+            async with message.channel.typing():
+                channels = await message.guild.fetch_channels()
 
+                count = {}
+
+                for channel in channels:
+                    if channel.type == discord.ChannelType.text:
+                        async for msg in channel.history(limit=None):
+                            if not message.author.bot:
+                                async with lock:
+                                    try:
+                                        count[channel.name] += 1
+                                    except KeyError:
+                                        count.update({channel.name: 1})
+
+            print(count)
+            keys = count.keys()
+            values = count.values()
+
+            plt.bar(keys, values, color="b")
+            plt.title("Descend Channel Messages", fontweight="bold", size=20)
+            plt.xticks(rotation=90)
+            plt.tight_layout()
+
+            plt.savefig('channel_usage.png')
+
+
+            # send the sorted list
+            sort = {k: v for k, v in sorted(count.items(), key=lambda item: item[1], reverse=True)}
+            text = []
+            i = 1
+            for k, v in sort.items():
+                text.append(f"{i}) `{k}` - Count: {v}\n")
+                i += 1
+
+            msgs = []
+            msg = ""
+            for e in text:
+                if len(msg + e) < 2000:
+                    msg += e
+                else:
+                    msgs.append(msg)
+                    msg = e
+            msgs.append(msg)
+
+
+            await a.delete()
+            await message.channel.send("__**Channel Messages**__")
+            for msg in msgs:
+                await message.channel.send(msg)
+
+            # sending them the file
+            await message.channel.send(file=discord.File('channel_usage.png'))
+
+            # delete file
+            os.remove('channel_usage.png')
