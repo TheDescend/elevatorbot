52ee0be7a360c0c35bd678fbd94fd2ad5b99ed5e
Daniel <daniel.jaek@gmail.com>
2020-11-19 17:20:03 +0100

    make learboard and fancier

--- commands/day1RaidRace.py
+++ commands/day1RaidRace.py
@@ -1,21 +1,15 @@
-import io
-
-import aiohttp
-import discord
-
 from commands.base_command import BaseCommand
-from functions.dataLoading import getStats, getProfile, getCharacterList, \
-    getAggregateStatsForChar, getInventoryBucket, getWeaponKills, returnManifestInfo, searchArmory, getAllGear, \
-    getItemDefinition, getArtifact, getCharacterGear, getCharacterGearAndPower, getPlayersPastPVE, getCharacterPastPVE
-from functions.database import lookupDiscordID, getToken, lookupSystem
+from functions.dataLoading import getProfile, getCharacterList, getCharacterPastPVE
 from functions.formating import embed_message
 from functions.network import getJSONfromURL
 from functions.roles import hasAdminOrDevPermissions
 from static.config import CLANID
-from static.dict import metricRaidCompletion, raidHashes
 
 import datetime
 import asyncio
+import io
+import aiohttp
+import discord
 
 
 class day1spam(BaseCommand):
@@ -32,7 +26,14 @@ class day1spam(BaseCommand):
             return
 
         # >>> CHANGE HERE FOR DIFFERENT DAY 1 HASHES <<<
-        activity_metric = 954805812
+        leaderboard_channel = 778993490981027881
+        activity_triumph = 2699580344
+        activity_triumph_encounters = {
+            4132628245: "Encounter 1",
+            4132628244: "Encounter 2",
+            4132628247: "Encounter 3",
+            4132628246: "the final Boss"
+        }
         activity_hashes = [3976949817, 910380154]
         cutoff_time = datetime.datetime(2020, 11, 22, 18, 0, tzinfo=datetime.timezone.utc)
         image_url = "https://www.bungie.net/img/destiny_content/pgcr/europa-raid-deep-stone-crypt.jpg"
@@ -52,7 +53,17 @@ class day1spam(BaseCommand):
                     await channel.send(file=discord.File(data, f'raid_image.png'))
 
         start = datetime.datetime.now()  # Need that for calculating total time. this time without utc timezone since bungie return doesnt care about that
-        finished_raid = []
+        leaderboard_msg = None
+        leaderboard_channel = client.get_channel(leaderboard_channel)
+
+        finished_raid = {}
+        finished_encounters = {}
+        clan_members = (await getJSONfromURL(f"https://www.bungie.net/Platform/GroupV2/{CLANID}/Members/"))["Response"]["results"]
+        for member in clan_members:
+            destinyID = member['destinyUserInfo']['membershipId']
+            finished_encounters[destinyID] = {}
+            for encounter in activity_triumph_encounters:
+                finished_encounters[destinyID][encounter] = 0
 
         # loop until raid race is done
         now = datetime.datetime.now(datetime.timezone.utc)
@@ -64,10 +75,20 @@ class day1spam(BaseCommand):
                     online.append((member['destinyUserInfo']['LastSeenDisplayName'], member['destinyUserInfo']['membershipId']))
 
             # loops through all online users
-            result = await asyncio.gather(*[look_for_completion(member, finished_raid, activity_metric, channel) for member in online])
+            result = await asyncio.gather(*[look_for_completion(member, finished_raid, finished_encounters, activity_triumph, activity_triumph_encounters, channel) for member in online])
             for res in result:
                 if res is not None:
-                    finished_raid.append(res)
+                    # check if all encounters are complete and if so save that
+                    all_done = True
+                    for encounter, complete in finished_encounters[res[1]].items():
+                        if complete == 0:
+                            all_done = False
+                            break
+                    if all_done:
+                        finished_raid[res] = datetime.datetime.now(datetime.timezone.utc)
+
+            # update leaderboard message
+            leaderboard_msg = await update_leaderboard(leaderboard_channel, leaderboard_msg, finished_raid, finished_encounters, activity_triumph_encounters, clan_members)
 
             # wait 1 min before checking again
             await asyncio.sleep(60)
@@ -75,31 +96,35 @@ class day1spam(BaseCommand):
             # set new now
             now = datetime.datetime.now(datetime.timezone.utc)
 
+
         async with channel.typing():
             if finished_raid:
                 # get total time spend in raid
                 completions = []
                 for member in finished_raid:
-                    name = member[0]
-                    destinyID = member[1]
-                    chars = (await getCharacterList(destinyID))[1]
+                    try:
+                        name = member[0]
+                        destinyID = member[1]
+                        chars = (await getCharacterList(destinyID))[1]
 
-                    # loop though activities
-                    time_spend = 0
-                    kills = 0
-                    deaths = 0
-                    for characterID in chars:
-                        async for activity in getCharacterPastPVE(destinyID, characterID, mode=4):
-                            if datetime.datetime.strptime(activity["period"], "%Y-%m-%dT%H:%M:%SZ") < start:
-                                break
+                        # loop though activities
+                        time_spend = 0
+                        kills = 0
+                        deaths = 0
+                        for characterID in chars:
+                            async for activity in getCharacterPastPVE(destinyID, characterID, mode=4):
+                                if datetime.datetime.strptime(activity["period"], "%Y-%m-%dT%H:%M:%SZ") < start:
+                                    break
 
-                            if activity["activityDetails"]["directorActivityHash"] in activity_hashes:
-                                time_spend += activity["values"]["activityDurationSeconds"]["basic"]["value"]
-                                kills += activity["values"]["kills"]["basic"]["value"]
-                                deaths += activity["values"]["deaths"]["basic"]["value"]
+                                if activity["activityDetails"]["directorActivityHash"] in activity_hashes:
+                                    time_spend += activity["values"]["activityDurationSeconds"]["basic"]["value"]
+                                    kills += activity["values"]["kills"]["basic"]["value"]
+                                    deaths += activity["values"]["deaths"]["basic"]["value"]
 
-                    # write the fancy text
-                    completions.append(f"""<:desc_circle_b:768906489464619008>**{name}** - Kills: *{int(kills):,}*, Deaths: *{int(deaths):,}*, Time: *{str(datetime.timedelta(seconds=time_spend))}*""")
+                        # write the fancy text
+                        completions.append(f"""<:desc_circle_b:768906489464619008>**{name}** - Kills: *{int(kills):,}*, Deaths: *{int(deaths):,}*, Time: *{str(datetime.timedelta(seconds=time_spend))}*""")
+                    except:
+                        print(f"Failed member {name}")
 
                 completion_text = "\n".join(completions)
                 msg = f"""The raid race is over :(. But some clan members managed to get a completion!\n⁣\n<:desc_logo_b:768907515193720874> __**Completions:**__\n{completion_text}"""
@@ -111,18 +136,84 @@ class day1spam(BaseCommand):
                 msg
             )
 
-            await channel.send(embed=embed)
+            stats = await channel.send(embed=embed)
+            await stats.pin()
 
 
-async def look_for_completion(member, finished_raid, activity_metric, channel):
+async def look_for_completion(member, finished_raid, finished_encounters, activity_triumph, activity_triumph_encounters, channel):
     name = member[0]
     destinyID = member[1]
 
     if member not in finished_raid:
         try:
-            metrics = await getProfile(destinyID, 1100)
-            if metrics["metrics"]["data"]["metrics"][str(activity_metric)]["objectiveProgress"]["progress"] > 0:
-                await channel.send(f"**{name}** has completed the raid. Congratulations! <:PeepoDPS:754785311489523754>")
-                return member
+            records = await getProfile(destinyID, 900)
+            record = records["profileRecords"]["data"]["records"][str(activity_triumph)]
+
+            # check each encounter
+            for encounter_hash, description in activity_triumph_encounters.items():
+                for record_objective_hash in record["objectives"]:
+                    if encounter_hash == record_objective_hash["objectiveHash"]:
+                        # abort if user has already completed that encounter
+                        if finished_encounters[destinyID][encounter_hash] == 0:
+                            # check if is complete
+                            if record_objective_hash["complete"]:
+                                finished_encounters[destinyID][encounter_hash] = 1
+                                await channel.send(f"**{name}** has completed {description} <:PeepoDPS:754785311489523754>")
+
+            return member
+
         except:
             pass
+
+async def update_leaderboard(leaderboard_channel, leaderboard_msg, finished_raid, finished_encounters, activity_triumph_encounters, clan_members):
+    running_raid = {}
+
+    # loop through clan members
+    for member in clan_members:
+        destinyID = member['destinyUserInfo']['membershipId']
+        name = member['destinyUserInfo']['LastSeenDisplayName']
+
+        # only check if member did not finish the raid. loop through their encounter completion
+        if (name, destinyID) not in finished_raid:
+            for encounter, complete in finished_encounters[destinyID].items():
+                if complete == 1:
+                    try:
+                        running_raid[(name, destinyID)].append(activity_triumph_encounters[encounter])
+                    except KeyError:
+                        running_raid[(name, destinyID)] = [activity_triumph_encounters[encounter]]
+
+    embed = embed_message(
+        "Race for World First / Day One"
+    )
+
+    if finished_raid:
+        embed.add_field(name="⁣", value=f"__Finished:__", inline=False)
+
+        sort = {k: v for k, v in sorted(finished_raid.items(), key=lambda item: item[1], reverse=False)}
+        times = []
+        names = []
+        for key, value in sort.items():
+            names.append(key[0])
+            times.append(f"{value.day}/{value.month} - {value.hour}:{value.minute:02d} UTC")
+
+        embed.add_field(name="Name", value="\n".join(names), inline=True)
+        embed.add_field(name="Time", value="\n".join(times), inline=True)
+
+    if running_raid:
+        embed.add_field(name="⁣", value=f"__Running:__", inline=False)
+
+        sort = {k: v for k, v in sorted(running_raid.items(), key=lambda item: len(item[1]), reverse=True)}
+        done = []
+        names = []
+        for key, value in sort.items():
+            names.append(key[0])
+            done.append(", ". join(value))
+
+        embed.add_field(name="Name", value="\n".join(names), inline=True)
+        embed.add_field(name="Completed", value="\n".join(done), inline=True)
+
+    if not leaderboard_msg:
+        leaderboard_msg = await leaderboard_channel.send(embed=embed)
+    else:
+        await leaderboard_msg.edit(embed=embed)
+    return leaderboard_msg
