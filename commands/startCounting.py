d3ea889352beb514cce3573fa11b587d6c013b90
Daniel <daniel.jaek@gmail.com>
2020-12-10 10:40:41 +0100

    disallow last user to send the next number

--- commands/startCounting.py
+++ commands/startCounting.py
@@ -18,15 +18,23 @@ class startCounting(BaseCommand):
         await message.channel.send("Start counting, first person has to begin with `1`")
 
         i = 1
+        last_msg = None
         while True:
             # wait for the next message in the channel
             def check(m):
                 return m.channel == message.channel
             msg = await client.wait_for('message', check=check)
 
+            # check if author was the one from the last successful count
+            if msg.author == last_msg:
+                await message.channel.send(f"You can't send two numbers in a row!")
+                continue
+
             # check if he counted up correctly
             if msg.content != str(i):
                 await message.channel.send(f"{msg.author.mention} ruined it at `{str(i-1)}`")
                 return
 
+            # count up and set new last user
+            last_msg = msg.author
             i += 1
