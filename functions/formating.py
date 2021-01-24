0bd8428bd27dfa88a5e81c9534aa4c0dee1251ff
Lukas Schmid <lukasschmid97@gmail.com>
2021-01-24 04:25:09 -0500

    docstring

--- functions/formating.py
+++ functions/formating.py
@@ -8,6 +8,7 @@ await message.channel.send(embed=embed_message(
 ))
 """
 def embed_message(title, desc="", footer=None):
+    """  Takes title and optionally *desc*ription/*footer* and returns an discord.Embed """
     embed = discord.Embed(
         title=title,
         description=desc,
