9b25a88b536776cb5cf00e7db5476879f4fc66db
Daniel <daniel.jaek@gmail.com>
2021-02-05 12:03:38 +0100

    make muteme fancier

--- commands/muteMe.py
+++ commands/muteMe.py
@@ -1,4 +1,8 @@
 import asyncio
+import random
+import aiohttp
+import io
+import discord
 
 from commands.base_command import BaseCommand
 from functions.roles import assignRolesToUser, removeRolesFromUser
@@ -18,11 +22,31 @@ class muteMe(BaseCommand):
         await message.channel.send("If you insist...")
         if message.author.id is not mentioned_user.id:
             await message.channel.send("I saw what you did there, that doesn't work here mate")
-        await message.author.send("I hope you enjoy your new role, the trial automatically ends after an half an hour 🙃")
+        await message.author.send("Introducing a new feature: **gambling!**")
+        await asyncio.sleep(1)
+        await message.author.send("Let me roll the dice for you, I can't wait to see if you win the jackpot")
+        await asyncio.sleep(2)
+        await message.author.send("_Rolling dice..._")
+        await asyncio.sleep(5)
+        timeout = random.choice([5, 10, 15, 30, 45, 60, 120])
+        if timeout == 120:
+            await message.author.send("**__!!! CONGRATULATIONS !!!__**")
+            async with aiohttp.ClientSession() as session:
+                async with session.get("https://media.istockphoto.com/videos/amazing-explosion-animation-with-text-congratulations-video-id1138902499?s=640x640") as resp:
+                    if resp.status == 200:
+                        data = io.BytesIO(await resp.read())
+                        await message.author.send(file=discord.File(data, 'congratulations.png'))
+
+            await message.author.send(f"You won the jackpot! That's a timeout of **{timeout} minutes** for you, enjoy!")
+        else:
+            await message.author.send(f"You won a timout of **{timeout} minutes**, congratulations!!!")
+            await asyncio.sleep(2)
+            await message.author.send("Better luck next time if you were hunting for the jackpot")
 
         # add muted role
         await assignRolesToUser([muted_role_id], message.author, message.guild)
 
         # remove muted role after an hour
-        await asyncio.sleep(60 * 30)
+        await asyncio.sleep(60 * timeout)
         await removeRolesFromUser([muted_role_id], message.author, message.guild)
+        await message.author.send("Sadly your victory is no more. Hope to see you back again soon!")
