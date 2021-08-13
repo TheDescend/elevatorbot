5cba71e12db2be9f41ae41b2a5a7817fb95abeac
Lukas Schmid <lukasschmid97@gmail.com>
2021-08-13 21:55:45 +0200

    refactoring

--- ElevatorBot/events/__init__.py
+++ ElevatorBot/events/__init__.py
@@ -3,4 +3,4 @@ from os.path import dirname, basename
 
 
 # Automatically import every class inside the events package. Magic! :D
-__all__ = [basename(f)[:-3] for f in listdir(dirname(__file__)) if f[-3:] == ".py" and not f.endswith("__init__.py")]
+__all__ = [basename(event_file)[:-3] for event_file in listdir(dirname(__file__)) if event_file.endswith(".py") and not event_file == "__init__.py"]
