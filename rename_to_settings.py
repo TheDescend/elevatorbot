f81f78a1726912212ed6e9bcadee0cae87bea50a
Daniel <daniel.jaek@gmail.com>
2021-08-18 00:49:25 +0200

    add sensible defaults to settings.py

--- rename_to_settings.py
+++ rename_to_settings.py
@@ -1,8 +1,8 @@
 # Fill out these settings, then rename the file to "settings.py" before running the app
 
 # This setting enables additional console log and print statements
-ENABLE_DEBUG_MODE: bool = None
+ENABLE_DEBUG_MODE: bool = False
 
 # Enabling this makes ElevatorBot sync all slash commands on startup
 # Enable to update changes made, but disable if many restarts are made to prevent rate limits
-SYNC_COMMANDS: bool = None
+SYNC_COMMANDS: bool = True
