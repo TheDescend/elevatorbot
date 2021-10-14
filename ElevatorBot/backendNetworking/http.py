80925fd3dadd7af539468d2747f2aca9dab2ac74
Daniel <daniel.jaek@gmail.com>
2021-10-14 18:25:37 +0200

    add limiter to backend calls

--- ElevatorBot/backendNetworking/http.py
+++ ElevatorBot/backendNetworking/http.py
@@ -1,12 +1,44 @@
+import asyncio
 import dataclasses
 import logging
+import time
 from typing import Optional
 
 import aiohttp
-import discord
 from aiohttp import ClientSession, ClientTimeout
+from dis_snek.models.discord_objects.user import Member
 
 from ElevatorBot.backendNetworking.results import BackendResult
+
+
+# the limiter object to not overload the backend
+class BackendRateLimiter:
+    RATE = 250
+    MAX_TOKENS = 10000
+
+    def __init__(self):
+        self.tokens = self.MAX_TOKENS
+        self.updated_at = time.monotonic()
+
+    async def wait_for_token(self):
+        """waits until a token becomes available"""
+        while self.tokens < 1:
+            self.add_new_tokens()
+            await asyncio.sleep(0.1)
+        assert self.tokens >= 1
+        self.tokens -= 1
+
+    def add_new_tokens(self):
+        """Adds a new token if eligible"""
+        now = time.monotonic()
+        time_since_update = now - self.updated_at
+        new_tokens = time_since_update * self.RATE
+        if self.tokens + new_tokens >= 1:
+            self.tokens = min(self.tokens + new_tokens, self.MAX_TOKENS)
+            self.updated_at = now
+
+
+backend_limiter = BackendRateLimiter()
 
 
 @dataclasses.dataclass
@@ -34,7 +66,9 @@ class BaseBackendConnection:
 
     # discord member
     # used for error message formatting
-    discord_member: Optional[discord.Member]
+    discord_member: Optional[Member]
+
+    limiter = backend_limiter
 
     def __bool__(self):
         """Bool function to test if this exist. Useful for testing if this class got returned and not BackendResult, can be returned on errors"""
@@ -44,23 +78,24 @@ class BaseBackendConnection:
     async def _backend_request(self, method: str, route: str, params: dict = None, data: dict = None) -> BackendResult:
         """Make a get request to the specified backend route and return the results"""
 
-        async with ClientSession(timeout=self.timeout) as session:
-            async with session.request(
-                method=method,
-                url=route,
-                params=params,
-                data=data,
-            ) as response:
-                result = await self.__backend_parse_response(response)
+        async with asyncio.Lock():
+            await self.limiter.wait_for_token()
 
-                # if an error occurred, already do the basic formatting
-                if not result:
-                    if self.discord_member:
-                        result.error_message = {
-                            "discord_member": self.discord_member
-                        }
+            async with ClientSession(timeout=self.timeout) as session:
+                async with session.request(
+                    method=method,
+                    url=route,
+                    params=params,
+                    data=data,
+                ) as response:
+                    result = await self.__backend_parse_response(response)
 
-                return result
+                    # if an error occurred, already do the basic formatting
+                    if not result:
+                        if self.discord_member:
+                            result.error_message = {"discord_member": self.discord_member}
+
+                    return result
 
     async def __backend_parse_response(self, response: aiohttp.ClientResponse) -> BackendResult:
         """Handle any errors and then return the content of the response"""
