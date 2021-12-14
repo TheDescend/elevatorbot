cb4e9e5eb0c1bbe2b239f579193f6f6fb99bb10e
Daniel <daniel.jaek@gmail.com>
2021-12-14 13:31:24 +0100

    small fixes

--- ElevatorBot/commands/misc/boosters.py
+++ ElevatorBot/commands/misc/boosters.py
@@ -1,4 +1,4 @@
-from dis_snek.models import InteractionContext, slash_command
+from dis_snek.models import InteractionContext, Member, TimestampStyles, slash_command
 
 from ElevatorBot.commands.base import BaseScale
 from ElevatorBot.misc.formating import embed_message
@@ -8,14 +8,16 @@ class Boosters(BaseScale):
     @slash_command(name="boosters", description="Prints all premium subscribers (boosters) of this discord server")
     async def _boosters(self, ctx: InteractionContext):
 
-        sorted_premium_subscribers = sorted(ctx.guild.premium_subscribers, key=lambda m: m.premium_since, reverse=True)
+        sorted_premium_subscribers: list[Member] = sorted(
+            ctx.guild.premium_subscribers, key=lambda m: m.premium_since, reverse=True
+        )
 
         embed = embed_message(
             f"{ctx.guild.name} Nitro Boosters",
             ",\n".join(
                 [
-                    "**" + f"{m.display_name:<30}" + "** since: " + m.premium_since.strftime("%d/%m/%Y, %H:%M")
-                    for m in sorted_premium_subscribers
+                    f"{member.mention} since {member.premium_since.format(TimestampStyles.ShortDateTime)}"
+                    for member in sorted_premium_subscribers
                 ]
             ),
         )
