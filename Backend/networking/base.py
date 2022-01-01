5f8562cf2e8a873a340d86005058cd4fa7a2b354
Daniel <daniel.jaek@gmail.com>
2022-01-01 19:43:33 +0100

    fix connection timeout failure

--- Backend/networking/base.py
+++ Backend/networking/base.py
@@ -76,6 +76,7 @@ class NetworkBase:
                         return WebResponse.from_dict(response.__dict__)
 
             except (asyncio.exceptions.TimeoutError, ConnectionResetError):
+                params = {} if params is None else params
                 self.logger.error("Timeout error for '%s'", f"{route}?{urlencode(params)}")
                 await asyncio.sleep(random.randrange(2, 6))
                 continue
