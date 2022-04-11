a2dda3c1780ea6eece59a732a2b832ad51907f8c
Daniel <daniel.jaek@gmail.com>
2022-04-11 17:21:58 +0200

    update ratelimiter to please bluenix

--- Shared/functions/ratelimiter.py
+++ Shared/functions/ratelimiter.py
@@ -7,36 +7,36 @@ import time
 class RateLimiter:
     """
     Gives out x tokens for network operations every y seconds
-    Adapted from https://gist.github.com/pquentin/5d8f5408cdad73e589d85ba509091741
     """
 
-    rate: int = 20  # how many requests per second - bungie allows 20/s
+    rate: float = 20  # how many requests per second - bungie allows 20/s
     max_tokens: int = 240  # how many requests can we save up - bungie limits after 250 in 10s, so will put that to 240
 
     tokens: float = dataclasses.field(init=False)
-    updated_at: float = dataclasses.field(init=False, default=time.monotonic())
+    updated_at: float = dataclasses.field(init=False, default=time.time())
 
     lock: asyncio.Lock = dataclasses.field(init=False, default=asyncio.Lock())
 
     def __post_init__(self):
         self.tokens = self.max_tokens
+        self.time_for_token = 1 / self.rate
 
     async def wait_for_token(self):
         """Waits until a token becomes available"""
 
         async with self.lock:
-            while self.tokens < 1:
-                self.add_new_tokens()
-                await asyncio.sleep(0.1)
-            assert self.tokens >= 1
+            if self.tokens == 0:
+                now = time.time()
+                time_since_update = now - self.updated_at
+
+                # sleep until at least one is ready
+                to_sleep = 0
+                if time_since_update < self.time_for_token:
+                    to_sleep = self.time_for_token - time_since_update
+                    await asyncio.sleep(to_sleep)
+                    time_since_update += to_sleep
+
+                self.tokens = min(self.tokens + time_since_update * self.rate, self.max_tokens)
+                self.updated_at = now + to_sleep
+
             self.tokens -= 1
-
-    def add_new_tokens(self):
-        """Adds a new token if eligible"""
-
-        now = time.monotonic()
-        time_since_update = now - self.updated_at
-        new_tokens = time_since_update * self.rate
-        if self.tokens + new_tokens >= 1:
-            self.tokens = min(self.tokens + new_tokens, self.max_tokens)
-            self.updated_at = now
