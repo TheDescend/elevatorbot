3fcdd057752b32c0e217bc2d2c999863f63ff8c2
Daniel <daniel.jaek@gmail.com>
2022-05-02 18:47:09 +0200

    fix ratelimiter

---
+++ Backend/tests/misc/test_ratelimiter.py
@@ -0,0 +1,40 @@
+import asyncio
+import time
+
+import pytest
+from anyio import create_task_group
+
+from Shared.functions.ratelimiter import RateLimiter
+
+
+@pytest.mark.asyncio
+async def test_ratelimiter():
+    limiter = RateLimiter(seconds=1, max_tokens=10)
+
+    # should need more than 2s for that many requests (has to wait for new tokens twice)
+    start = time.perf_counter()
+    async with create_task_group() as tg:
+        for _ in range(30):
+            tg.start_soon(lambda: limiter.wait_for_token())
+    end = time.perf_counter()
+    assert (end - start) >= 2
+
+    # now it should need more than 3s for that many requests (has to wait for new tokens thrice)
+    start = time.perf_counter()
+    async with create_task_group() as tg:
+        for _ in range(30):
+            tg.start_soon(lambda: limiter.wait_for_token())
+    end = time.perf_counter()
+    assert (end - start) >= 3
+
+    # this should go by instantly
+    limiter = RateLimiter(seconds=10000, max_tokens=10000)
+    start = time.perf_counter()
+    async with create_task_group() as tg:
+        for _ in range(10000):
+            tg.start_soon(lambda: limiter.wait_for_token())
+    end = time.perf_counter()
+    assert (end - start) <= 1
+
+
+asyncio.run(test_ratelimiter())
--- Shared/functions/ratelimiter.py
+++ Shared/functions/ratelimiter.py
@@ -9,34 +9,29 @@ class RateLimiter:
     Gives out x tokens for network operations every y seconds
     """
 
-    rate: float = 20  # how many requests per second - bungie allows 20/s
     max_tokens: int = 240  # how many requests can we save up - bungie limits after 250 in 10s, so will put that to 240
+    seconds: int = 10  # in how many seconds those requests are allowed
 
     tokens: float = dataclasses.field(init=False)
-    updated_at: float = dataclasses.field(init=False, default=time.time())
+    updated_at: float = dataclasses.field(init=False)
 
     lock: asyncio.Lock = dataclasses.field(init=False, default=asyncio.Lock())
 
     def __post_init__(self):
         self.tokens = self.max_tokens
-        self.time_for_token = 1 / self.rate
 
     async def wait_for_token(self):
         """Waits until a token becomes available"""
 
         async with self.lock:
             if self.tokens == 0:
-                now = time.time()
-                time_since_update = now - self.updated_at
+                current_time = time.time()
+                if (missing := current_time - self.updated_at) < self.seconds:
+                    await asyncio.sleep(self.seconds - missing)
+                self.tokens = self.max_tokens
 
-                # sleep until at least one is ready
-                to_sleep = 0
-                if time_since_update < self.time_for_token:
-                    to_sleep = self.time_for_token - time_since_update
-                    await asyncio.sleep(to_sleep)
-                    time_since_update += to_sleep
-
-                self.tokens = min(self.tokens + time_since_update * self.rate, self.max_tokens)
-                self.updated_at = now + to_sleep
+            if self.tokens == self.max_tokens:
+                # the first request is made, start the timer when that should fill back up
+                self.updated_at = time.time()
 
             self.tokens -= 1
