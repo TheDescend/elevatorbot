4ad19759af6d4a374fc41af3d9926e2b78b47c85
Daniel <daniel.jaek@gmail.com>
2022-06-03 10:32:19 +0200

    add error handler

--- Website/pages/_app.js
+++ Website/pages/_app.js
@@ -3,6 +3,7 @@ import {ThemeProvider} from "next-themes";
 import {SessionProvider} from 'next-auth/react'
 import {SWRConfig} from 'swr'
 import NodeCache from "node-cache";
+import ErrorBoundary from "../components/errors";
 
 
 global.customCache = new NodeCache({
@@ -12,17 +13,19 @@ global.customCache = new NodeCache({
 
 export default function App({Component, pageProps}) {
     return (
-        <SessionProvider session={pageProps.session}>
-            <SWRConfig value={{provider: () => new Map()}}>
-                <ThemeProvider
-                    forcedTheme={Component.theme || undefined}
-                    attribute="class"
-                    disableTransitionOnChange={true}
-                    defaultTheme="dark"
-                >
-                    <Component {...pageProps} />
-                </ThemeProvider>
-            </SWRConfig>
-        </SessionProvider>
+        <ErrorBoundary>
+            <SessionProvider session={pageProps.session}>
+                <SWRConfig value={{provider: () => new Map()}}>
+                    <ThemeProvider
+                        forcedTheme={Component.theme || undefined}
+                        attribute="class"
+                        disableTransitionOnChange={true}
+                        defaultTheme="dark"
+                    >
+                        <Component {...pageProps} />
+                    </ThemeProvider>
+                </SWRConfig>
+            </SessionProvider>
+        </ErrorBoundary>
     )
 }
